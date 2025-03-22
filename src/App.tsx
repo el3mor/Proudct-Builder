aef6a2e8f2c7a5594054594edc17660967b0fa0f
elamoor <elamoor82@gmail.com>
2025-03-22 09:37:48 +0200

    first

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='bg-blue-500 w-10 h-10 flex items-center justify-center'>
+    <div className='bg-red-500 w-6 h-6'></div>
+    </div>
+  )
+}
+
+export default App
